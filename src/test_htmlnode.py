8063d2de9aea0a375a682257b51e91a211acf2f6
anth-riv <anthony.rivoiron@gmail.com>
2024-03-21 14:55:26 +0100

    HTMLnode and Leafnode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,27 @@
+import unittest
+from htmlnode import HTMLNode, LeafNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://www.google.fr"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://www.google.fr"',
+        )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
+if __name__ == "__main__":
+    unittest.main()
